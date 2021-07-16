6afa9054b6fbbeece0c181c5c77beb256e148881
Bryce Conner <bryce.webmaster@gmail.com>
2021-07-16 00:05:20 -0400

    Version 1.2: finished logic portion

--- tic-tac-toe/src/App3.vue
+++ tic-tac-toe/src/App3.vue
@@ -1,5 +1,7 @@
 <template>
 <div class="board">
+    <div v-if="isWinner('x')"><h3>x wins</h3></div>
+    <div v-if="isWinner('o')"><h3>o wins</h3></div>
 <form class="board">
   <div v-for="i in 9" :key="i">
     <button 
@@ -8,10 +10,8 @@
       <img height="40" width="40" :src="tokenImg(i-1)"/>
     </button>
   </div>
-  <button @click="restart">New Game</button>
+  <button @click="restart">clear</button>
  </form>
-   <div v-if="isWinner('x') && !isWinner('o')"><h2>x wins</h2></div>
-   <div v-if="isWinner('o') && !isWinner('x')"><h2>o wins</h2></div>
  </div>
 </template>
 
@@ -21,18 +21,18 @@ var board = [0,0,0,0,0,0,0,0,0];
 var currentPlayer = 1;
 const path = ["img/0.png","img/x.png","img/o.png"];
 const token = [0, "x", "o"];
-const moveData = [ [0, [4, 8]],  [0, [1, 2]], 
-                   [0, [3, 6]],  [1, [0, 2]], 
-                   [1, [4, 7]],  [2, [0, 1]], 
-                   [2, [4, 6]],  [2, [5, 8]],
-                   [3, [0, 6]],  [3, [4, 5]], 
-                   [4, [0, 8]],  [4, [1, 7]], 
-                   [4, [2, 6]],  [4, [3, 5]], 
-                   [5, [2, 8]],  [5, [3, 4]],
-                   [6, [0, 3]],  [6, [7, 8]],
-                   [6, [2, 4]],  [7, [6, 8]],
-                   [7, [1, 4]],  [8, [2, 5]],
-                   [8, [6, 7]],  [8, [0, 4]] ];
+const moveData = [[0, [0, [4, 8]]], [1, [0, [1, 2]]], 
+                  [2, [0, [3, 6]]], [3, [1, [0, 2]]], 
+                  [4, [1, [4, 7]]], [5, [2, [0, 1]]], 
+                  [6, [2, [4, 6]]], [7, [2, [5, 8]]],
+                  [8, [3, [0, 6]]], [9, [3, [4, 5]]], 
+                  [10, [4, [0, 8]]], [11, [4, [1, 7]]], 
+                  [12, [4, [2, 6]]], [13, [4, [3, 5]]], 
+                  [14, [5, [2, 8]]], [15, [5, [3, 4]]],
+                  [16, [6, [0, 3]]], [17, [6, [7, 8]]],
+                  [18, [6, [2, 4]]], [19, [7, [6, 8]]],
+                  [20, [7, [1, 4]]], [21, [8, [2, 5]]],
+                  [22, [8, [6, 7]]], [23, [8, [0, 4]]] ];
 var gameMoves = [];
 export default {
   name: "App",
@@ -101,24 +101,23 @@ export default {
       return flag;
     },
     boardLogic(x, y, z){
-      if ((this.board[y] === this.board[z]) && this.board[y] != 0)
+      if ((this.board[y] === this.board[z]) && this.board[y])
         if (this.board[y] === 'o')
           return [moveData[x][0], 2];
-        else 
+        else if (this.board[y] === 'x')
           return [moveData[x][0], 1];
       else return [];
     },
     selectSquare(i) {
       var m = -1, movePlaces;
       var bestMove = [], betterMovePlaces = [];
-      if (board[i] != 0 || gameDone) return;  
-
+      if (board[i]||gameDone) return;  // bad click or possibly bad recursion
       board[i] = this.token[this.currentPlayer];
       gameMoves.push(i);
       this.currentPlayer = 3-this.currentPlayer;
       // logic for 'o'
-      if (this.currentPlayer == 2 && !gameDone) {
-        if (gameMoves.length<3 && this.board[4]=='x') {
+      if (this.currentPlayer == 2) {
+        if (gameMoves.length<3) {
           // x moves center.   o plays in corner
           movePlaces = [0, 2, 6, 8];
           m = movePlaces[Math.floor(Math.random()*movePlaces.length)];
@@ -131,20 +130,24 @@ export default {
           var n = [];
           for (x=0; x<moveData.length; x++) {
             n = this.boardLogic(x, moveData[x][1][0], moveData[x][1][1]);
-            if (!n || !n.length || this.board[n[0]] != 0) continue;
+            console.log(n);
+            if (!n || !n.length) continue;
             if (n[1]==this.currentPlayer) bestMove.push(n[0]);
             else betterMovePlaces.push(n[0]);
           }
         }
         if (bestMove.length) m = bestMove[Math.floor(Math.random() * bestMove.length)];
-        else if (betterMovePlaces.length) m = betterMovePlaces[Math.floor(Math.random()*betterMovePlaces.length)];
+        else if (betterMovePlaces.length) m = betterMovePlaces[Math.floor(Math.random()*betterMovePlaces)];
         else if (movePlaces.length) m = movePlaces[Math.floor(Math.random() * movePlaces.length)];
         else m = movePlaces[Math.floor(Math.random()*movePlaces.length)];
         console.log("Possible Moves: "+movePlaces);
-        console.log("Better Moves:"+betterMovePlaces);
+        console.log("Move Candidates:"+betterMovePlaces);
         console.log("Best: "+bestMove);
-        console.log("Chosen: "+m);
-        if (!gameDone) this.selectSquare(m);
+        console.log("Chosen"+m);
+          this.selectSquare(m);
+//        gameMoves.push(m);
+//        board[m] = this.token[this.currentPlayer];
+//        this.currentPlayer = 3-this.currentPlayer;
       } 
     },
   },
