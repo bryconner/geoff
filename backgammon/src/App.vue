a1f2c9e41ceba6b69238467caf1e18d629ceb9de
Bryce Conner <bryce.webmaster@gmail.com>
2021-07-31 15:36:48 -0400

    backgammon 0.10 - change from default template to start of editing

--- backgammon/src/App.vue
+++ backgammon/src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="board">
+    <div id="section1">
+    </div>
+    <div id="section2">
+    </div>
+    <div id="section3">
+    </div>
+    <div id="section4">
+    </div>
+  </div>
+  <div id="panel">
+  </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
