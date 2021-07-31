79e460fb5eeb9210f351c380189361392dbdc5c2
Bryce Conner <bryce.webmaster@gmail.com>
2021-07-31 19:06:36 -0400

    just another update

--- backgammon/src/App.vue
+++ backgammon/src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="board">
     <div id="section1">
+      <div v-for="i in 6" :key="i">
+        <iconx name="pip" :class="pip+i%2" :id="pip+(i+0)"></iconx>
+      </div>
     </div>
     <div id="section2">
+      <div v-for="i in 6" :key="i">
+        <iconx name="pip" :class="pip+i%2" :id="pip+(i+6)"></iconx>
+      </div>
+      
     </div>
     <div id="section3">
+      <div v-for="i in 6" :key="i">
+        <iconx name="pip" :class="pip+i%2" :id="pip+(i+12)"></iconx>
+      </div>
     </div>
     <div id="section4">
+      <div v-for="i in 6" :key="i">
+        <iconx name="pip" :class="pip+i%2" :id="pip+(i+18)"></iconx>
+      </div>
     </div>
   </div>
   <div id="panel">
